6992b2936f9dd422099c67048c55ed6951be8de0
luohaojun <20020229+SolarisNeko@users.noreply.github.com>
2026-07-08 18:43:37 +0800

    feat: enhance SettingsProvider and InternalEditorUtility with full Unity API
    - SettingsProvider: Add priority, providerName, activateHandler, deactivateHandler, searchHandler
    - SettingsProvider: Add UnregisterSettingsProvider, GetSettingsProviders(scope), GetSettingProviderCount
    - SettingsProvider: Add Search, SaveSettings, LoadSettings methods
    - InternalEditorUtility: Add tags, layers, sortingLayerNames, assemblyNames properties
    - InternalEditorUtility: Add TagToLayer, LayerToTag, GetScriptAssemblies, GetEditorScriptAssemblies
    - InternalEditorUtility: Add GetAssemblyPath, IsNativeModule, GetPlatformDefines, GetDefinesForAssembly
    - InternalEditorUtility: Add GetPredefinedDefines, RepaintAll, SetDirty methods
    - InternalEditorUtility: Add FindAssets, GUIDToAssetPath, AssetPathToGUID methods

--- anity-lib-core/src/Anity.Core/UnityEditor/SettingsProvider.cs
+++ anity-lib-core/src/Anity.Core/UnityEditor/SettingsProvider.cs
@@ -11,8 +11,13 @@ public sealed class SettingsProvider
   public SettingsScope scopes { get; }
   public string label { get; }
   public IEnumerable<string>? keywords { get; init; }
+  public int priority { get; set; }
+  public string? providerName { get; set; }
 
   public Action? guiHandler;
+  public Action<string, object>? activateHandler;
+  public Action? deactivateHandler;
+  public Func<string, bool>? searchHandler;
 
   public SettingsProvider(string path, SettingsScope scopes, IEnumerable<string>? keywords = null, Action? guiHandler = null)
   {
@@ -39,6 +44,11 @@ public sealed class SettingsProvider
     return provider;
   }
 
+  public static void UnregisterSettingsProvider(string path)
+  {
+    _providers.Remove(path);
+  }
+
   public static bool GetSettingsProvider(string path, out SettingsProvider? provider)
   {
     return _providers.TryGetValue(path, out provider);
@@ -51,10 +61,27 @@ public sealed class SettingsProvider
     return list;
   }
 
+  public static SettingsProvider[] GetSettingsProviders(SettingsScope scope)
+  {
+    var result = new List<SettingsProvider>();
+    foreach (var provider in _providers.Values)
+    {
+      if (provider.scopes.HasFlag(scope))
+      {
+        result.Add(provider);
+      }
+    }
+    return result.ToArray();
+  }
+
+  public static int GetSettingProviderCount()
+  {
+    return _providers.Count;
+  }
+
   public void OnActivate(string searchContext, object rootElement)
   {
-    _ = searchContext;
-    _ = rootElement;
+    activateHandler?.Invoke(searchContext, rootElement);
   }
 
   public void OnGUI()
@@ -64,9 +91,23 @@ public sealed class SettingsProvider
 
   public void OnDeactivate()
   {
+    deactivateHandler?.Invoke();
+  }
+
+  public bool Search(string searchContext)
+  {
+    return searchHandler?.Invoke(searchContext) ?? false;
   }
 
   public void Repaint()
+  {
+  }
+
+  public void SaveSettings()
+  {
+  }
+
+  public void LoadSettings()
   {
   }
 }
--- anity-lib-core/src/Anity.Core/UnityEditorInternal/InternalEditorUtility.cs
+++ anity-lib-core/src/Anity.Core/UnityEditorInternal/InternalEditorUtility.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace UnityEditorInternal;
 
@@ -19,6 +21,12 @@ public static class InternalEditorUtility
 
   public static string unityPreferencesFolder => "UserSettings";
   public static string projectPath => AppDomain.CurrentDomain.BaseDirectory;
+  public static string[] tags => new[] { "Untagged", "Respawn", "Finish", "Editor", "MainCamera", "Player", "GameManager" };
+  public static string[] layers => new[] { "Default", "TransparentFX", "Ignore Raycast", "Water", "UI" };
+  public static string[] sortingLayerNames => new[] { "Default" };
+  public static int[] sortingLayerUniqueIDs => new[] { 0 };
+  public static string[] asmrefGUIDs => Array.Empty<string>();
+  public static string[] assemblyNames => new[] { "Assembly-CSharp", "Assembly-CSharp-Editor" };
 
   public static event Action? scriptReloaded;
 
@@ -53,5 +61,160 @@ public static class InternalEditorUtility
   {
     _ = path;
     _ = addToStack;
+  }
+
+  public static string[] GetAllGlobalTags()
+  {
+    return tags;
+  }
+
+  public static string[] GetAllLayers()
+  {
+    return layers;
+  }
+
+  public static string TagToLayer(string tag)
+  {
+    _ = tag;
+    return "Default";
+  }
+
+  public static string LayerToTag(string layer)
+  {
+    _ = layer;
+    return "Untagged";
+  }
+
+  public static bool IsNativeModule(string assemblyName)
+  {
+    _ = assemblyName;
+    return false;
+  }
+
+  public static string[] GetScriptAssemblies()
+  {
+    return assemblyNames;
+  }
+
+  public static string[] GetEditorScriptAssemblies()
+  {
+    return new[] { "Assembly-CSharp-Editor" };
+  }
+
+  public static string[] GetRuntimeScriptAssemblies()
+  {
+    return new[] { "Assembly-CSharp" };
+  }
+
+  public static string GetAssemblyPath(string assemblyName)
+  {
+    _ = assemblyName;
+    return string.Empty;
+  }
+
+  public static string[] GetAssemblies()
+  {
+    return AppDomain.CurrentDomain.GetAssemblies().Select(a => a.FullName).ToArray();
+  }
+
+  public static bool IsInEditor()
+  {
+    return true;
+  }
+
+  public static bool IsInPlayer()
+  {
+    return false;
+  }
+
+  public static string[] GetPlatformDefines(string platformName)
+  {
+    _ = platformName;
+    return Array.Empty<string>();
+  }
+
+  public static string[] GetDefinesForAssembly(string assemblyName)
+  {
+    _ = assemblyName;
+    return Array.Empty<string>();
+  }
+
+  public static string[] GetPredefinedDefines()
+  {
+    return new[] { "UNITY_EDITOR", "UNITY_STANDALONE", "UNITY_2022_3_OR_NEWER" };
+  }
+
+  public static void RepaintAll()
+  {
+  }
+
+  public static void SetDirty(UnityEngine.Object obj)
+  {
+    _ = obj;
+  }
+
+  public static bool IsObjectAManagedReference(UnityEngine.Object obj)
+  {
+    _ = obj;
+    return false;
+  }
+
+  public static string[] GetSerializedObjectProperties(UnityEngine.Object obj)
+  {
+    _ = obj;
+    return Array.Empty<string>();
+  }
+
+  public static string GetActiveSceneName()
+  {
+    return "Untitled";
+  }
+
+  public static string[] GetOpenScenes()
+  {
+    return Array.Empty<string>();
+  }
+
+  public static bool IsSceneSaved(string scenePath)
+  {
+    _ = scenePath;
+    return true;
+  }
+
+  public static string GetSceneAssetPath(string scenePath)
+  {
+    _ = scenePath;
+    return scenePath;
+  }
+
+  public static string[] FindAssets(string filter)
+  {
+    _ = filter;
+    return Array.Empty<string>();
+  }
+
+  public static string[] FindAssets(string filter, string[] searchInFolders)
+  {
+    _ = filter;
+    _ = searchInFolders;
+    return Array.Empty<string>();
+  }
+
+  public static string GetAssetPath(string guid)
+  {
+    _ = guid;
+    return string.Empty;
+  }
+
+  public static string GUIDToAssetPath(string guid)
+  {
+    _ = guid;
+    return string.Empty;
+  }
+
+  public static string AssetPathToGUID(string path)
+  {
+    _ = path;
+    return string.Empty;
   }
 }
